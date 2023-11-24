76d17d706ae0aa80692b6f25337a819be1d7f0e7
NanQin555 <31352470642qq.com>
2023-11-24 13:13:45 +0800

    <feat>: 增加了RISC-V相关指令

--- backend/AsmPrinter.hpp
+++ backend/AsmPrinter.hpp
@@ -81,3 +81,84 @@ enum Register {
     t5 = x30,
     t6 = x31
 };
+
+//list of RISC_V instructions
+enum RISC_V {
+    //算数运算
+    Add, //加法 add rd, rs1, rs2
+    Addw,
+    Addi, // addi rd, rs1, imm  
+    Addiw,
+    Sub, //减法 sub rd, rs1, rs2
+    Subw,
+    Mul, //乘法 mul rd, rs1, rs2
+    Mulh,
+    Mulw,
+    Div, //除法 div rd, rs1, rs2
+    Divw,
+    Rem, //取余 rem rd, rs1, rs2
+    Remw,
+
+    Beq, //分支 beq rs1, rs2, offset
+    Beqz, // == 0
+    Bge, // >=
+    Bgez,
+    Bgt, // >
+    Bgtz,
+    Ble, // <=
+    Blez,
+    Blt, // <
+    Bltz,
+    Bne, // !=
+    Bnez,
+    J, //跳转 j offset
+    Jr, //跳转 jr rs1
+    Jal, //跳转并保存返回地址  jal offset
+    Jalr,// 跳转并保存返回地址 jalr rs1
+
+    La, //加载地址 la rd, label
+    Li, //加载立即数 li rd, imm
+    Lb, //加载 lb rd, offset(rs1)
+    Lh,
+    Lw,
+    Ld,
+    Sb, //存储 sb rs2, offset(rs1)
+    Sh,
+    Sw,
+    Sd,
+
+    //Nop, //空指令
+    //Auipc,
+    Mv, //移动 mv rd, rs1
+    Ret, //返回 ret
+
+    And, //按位与 and rd, rs1, rs2
+    Andi,
+    Or, //按位或 or rd, rs1, rs2
+    Ori,
+    Not, //按位取反 not rd, rs1
+    Xor, //按位异或 xor rd, rs1, rs2
+    Xori,
+    //Neg, //取负数
+    //Negw,
+
+
+    Sll, //逻辑左移 sll rd, rs1, rs2
+    Slli,
+    Slliw,
+    Sllw,
+    Srl, //逻辑右移 srl rd, rs1, rs2
+    Srlw,
+    Srli, //逻辑右移立即数 srli rd, rs1, imm
+    Srliw,
+    Sra, //算术右移 sra rd, rs1, rs2
+    Sraw, 
+    Srai, //算术右移立即数 srai rd, rs1, imm
+    Sraiw,
+
+    Slt, //有符号比较 slt rd, rs1, rs2
+    Slti,
+    Sltz,
+    Snez
+    
+};
