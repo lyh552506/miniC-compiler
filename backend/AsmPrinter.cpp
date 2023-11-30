7995167ce7a0d1fbcea1d170a2877fefb742b11b
NanQin555 <31352470642qq.com>
2023-11-30 20:43:25 +0800

    <feature>: 完成机器指令的打印函数

--- backend/AsmPrinter.cpp
+++ backend/AsmPrinter.cpp
@@ -4,7 +4,7 @@
 
 void printToLL() {
     std::ofstream outputFile("output.ll", std::ios::app); // 以追加模式打开文件
-    BinaryInst inst = BinaryInst(nullptr, BinaryInst::Add, new MachineOperand(MachineOperand::REG, 1), new MachineOperand(MachineOperand::REG, 2), new MachineOperand(MachineOperand::REG, 3));
+    LoadInst inst = LoadInst(nullptr, LoadInst::lw, new MachineOperand(MachineOperand::REG, 1), new MachineOperand(MachineOperand::REG, 2));
     if (outputFile.is_open()) {
         inst.print(outputFile);
         
--- backend/MachineCode.hpp
+++ backend/MachineCode.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<iostream>  
+#include"../lib/MagicEnum.hpp"
 
 class MachineUnit;
 class MachineFunction;
@@ -14,15 +16,73 @@ private:
     int type;
     int val;
     std::string lable;
-    int regnum;
 
 public:
 enum OperandType{
     REG, 
     IMM, 
-    MEM, // MEM[REG + IMM]
+    //MEM, // MEM[REG + IMM]
     LABEL //address lable
 };
+
+//The list of all registers
+enum Register {
+    x0 = 0, x1 = 1, x2 = 2, x3 = 3, x4 = 4, x5 = 5, x6 = 6, 
+    x7 = 7, x8 = 8, x9 = 9, x10 = 10, x11 = 11, x12 = 12, x13 = 13, 
+    x14 = 14, x15 = 15, x16 = 16, x17 = 17, x18 = 18, x19 = 19, 
+    x20 = 20, x21 = 21, x22 = 22, x23 = 23, x24 = 24, x25 = 25, 
+    x26 = 26, x27 = 27, x28 = 28, x29 = 29, x30 = 30, x31 = 31,
+
+    //0值寄存器
+    zero = x0,
+    //返回地址
+    ra = x1,
+    //栈指针
+    sp = x2,
+    //全局指针，通用指针
+    gp = x3,
+    //线程指针
+    tp = x4,
+    //临时数据或备用链接寄存器
+    t0 = x5,
+    //临时数据
+    t1 = x6,
+    t2 = x7,
+    //保存寄存器或帧指针
+    s0 = x8,
+    fp = x8,
+    //保存寄存器
+    s1 = x9,
+    //函数参数或返回值
+    a0 = x10,
+    a1 = x11,
+    //函数传递参数
+    a2 = x12,
+    a3 = x13,
+    a4 = x14,
+    a5 = x15,
+    a6 = x16,
+    a7 = x17,
+    //保存寄存器
+    s2 = x18,
+    s3 = x19,
+    s4 = x20,
+    s5 = x21,
+    s6 = x22,
+    s7 = x23,
+    s8 = x24,
+    s9 = x25,
+    s10 = x26,
+    s11 = x27,
+    //临时数据
+    t3 = x28,
+    t4 = x29,
+    t5 = x30,
+    t6 = x31
+};
+private:    
+    Register regnum;
+public:    
     MachineOperand();
     MachineOperand(int type, int val);
 
@@ -39,10 +99,19 @@ enum OperandType{
     int getType();
     int getVal();
     int getRegnum();
+    //auto getRegName();
+    auto MachineOperand::getRegName() {
+    return magic_enum::enum_name (this->regnum);
+}
     std::string getlable();
 
     void setParent(MachineInst *parent);
-    void setReg(int regnum);
+    void setReg(Register regnum);
+
+    friend std::ostream& operator<<(std::ostream& os, MachineOperand& operand) {
+        os << operand.getRegName();
+        return os;
+    }
 };
 
 class MachineInst
@@ -50,13 +119,12 @@ class MachineInst
 protected:
     MachineBlock *parent;
     int type;
-    int opcode;
     MachineOperand *rd;
     MachineOperand *rs1;
     MachineOperand *rs2;
     int num;
-    std::vector<MachineOperand *> defList;
-    std::vector<MachineOperand *> useList;
+    // std::vector<MachineOperand *> defList;
+    // std::vector<MachineOperand *> useList;
 public:
 enum InstType {
     Binary,
@@ -80,41 +148,45 @@ class BinaryInst : public MachineInst
 {  
 public:
 enum Binary_Inst {
+    //and, or, xor 有问题
     //算数运算
-    Add, //加法 add rd, rs1, rs2
-    Addw,
-    Addi, // addi rd, rs1, imm  
-    Addiw,
-    Sub, //减法 sub rd, rs1, rs2
-    Subw,
-    Mul, //乘法 mul rd, rs1, rs2
-    Mulh,
-    Mulw,
-    Div, //除法 div rd, rs1, rs2
-    Divw,
-    Rem, //取余 rem rd, rs1, rs2
-    Remw,
-    Sll, //逻辑左移 sll rd, rs1, rs2
-    Slli,
-    Slliw,
-    Sllw,
-    Srl, //逻辑右移 srl rd, rs1, rs2
-    Srlw,
-    Srli, //逻辑右移立即数 srli rd, rs1, imm
-    Srliw,
-    Sra, //算术右移 sra rd, rs1, rs2
-    Sraw, 
-    Srai, //算术右移立即数 srai rd, rs1, imm
-    Sraiw,
+    add, //加法 add rd, rs1, rs2
+    addw,
+    addi, // addi rd, rs1, imm  
+    addiw,
+    sub, //减法 sub rd, rs1, rs2
+    subw,
+    mul, //乘法 mul rd, rs1, rs2
+    mulh,
+    mulw,
+    div, //除法 div rd, rs1, rs2
+    divw,
+    rem, //取余 rem rd, rs1, rs2
+    remw,
+    sll, //逻辑左移 sll rd, rs1, rs2
+    slli,
+    slliw,
+    sllw,
+    srl, //逻辑右移 srl rd, rs1, rs2
+    srlw,
+    srli, //逻辑右移立即数 srli rd, rs1, imm
+    srliw,
+    sra, //算术右移 sra rd, rs1, rs2
+    sraw, 
+    srai, //算术右移立即数 srai rd, rs1, imm
+    sraiw,
     And, //按位与 and rd, rs1, rs2
-    Andi,
+    andi,
     Or, //按位或 or rd, rs1, rs2
-    Ori,
+    ori,
     Xor, //按位异或 xor rd, rs1, rs2
-    Xori
+    xori
 };
+private:
+    Binary_Inst opcode;
 
-    BinaryInst(MachineBlock *parent, int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
+public:
+    BinaryInst(MachineBlock *parent, Binary_Inst opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
     void print(std::ofstream &outputFile);
 };
 
@@ -122,48 +194,60 @@ class LoadInst : public MachineInst
 {
 public:
 enum Load_Inst {
-    Lb, //加载 lb rd, offset(rs1)
-    Lh,
-    Lw,
-    Ld
+    lb, //加载 lb rd, offset(rs1)
+    lh,
+    lw,
+    ld,
+    li, //加载立即数 li rd, imm
+    la //加载地址 la rd, label
 };
-    LoadInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
+private: 
+    Load_Inst opcode;
+public:
+    LoadInst(MachineBlock *parent, Load_Inst opcode, MachineOperand *rd, MachineOperand *rs1);
+    void print(std::ofstream &outputFile);
 };
 
 class StoreInst : public MachineInst
 {
 public:
 enum Store_Inst {
-    Sb, //存储 sb rs2, offset(rs1)
-    Sh,
-    Sw,
-    Sd
+    sb, //存储 sb rs2, offset(rs1)
+    sh,
+    sw,
+    sd
 };
-    StoreInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
-
+private:
+    Store_Inst opcode;
+public:
+    StoreInst(MachineBlock *parent, Store_Inst opcode, MachineOperand *rs2, MachineOperand *rs1, MachineOperand *offset);
+    void print(std::ofstream &outputFile);
 };
 
 class BranchInst : public MachineInst
 {
 public:
 enum Branch_Inst {
-    Beq, //分支 beq rs1, rs2, offset
-    Beqz, // == 0
-    Bge, // >=
-    Bgez,
-    Bgt, // >
-    Bgtz,
-    Ble, // <=
-    Blez,
-    Blt, // <
-    Bltz,
-    Bne, // !=
-    Bnez
+    beq, //分支 beq rs1, rs2, offset
+    bge, // >=
+    bgt, // >
+    ble, // <=
+    blt, // <
+    bne, // !=  bne = 6;
+
+    beqz, // == 0 beqz rs1, offset
+    bgez,
+    bgtz,
+    blez,
+    bltz,
+    bnez
 };
-    BranchInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
+private:
+    Branch_Inst opcode;
+public:
+    BranchInst(MachineBlock *parent, Branch_Inst opcode, MachineOperand *rs1, MachineOperand *rs2);
+    BranchInst(MachineBlock *parent, Branch_Inst opcode, MachineOperand *rs1);
+    void print(std::ofstream &outputFile);
 
 };
 
@@ -171,28 +255,37 @@ class JumpInst : public MachineInst
 {
 public:
 enum Jump_Inst {
-    J, //跳转 j offset
-    Jr, //跳转 jr rs1
-    Jal, //跳转并保存返回地址  jal offset
-    Jalr,// 跳转并保存返回地址 jalr rs1
+    jal, //跳转并保存返回地址  jal rd, offset
+    jalr,// 跳转并保存返回地址 jalr rd, offset(rs1)
+    j, //跳转 j offset
+    jr, //跳转 jr rs1
 };
-    JumpInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
-
+private:
+    Jump_Inst opcode;
+public:
+    JumpInst(MachineBlock *parent, Jump_Inst opcode, MachineOperand *rd);
+    JumpInst(MachineBlock *parent, Jump_Inst opcode, MachineOperand *rd, MachineOperand *rs1);
+    JumpInst(MachineBlock *parent, Jump_Inst opcode);
+    JumpInst(MachineBlock *parent, Jump_Inst opcode, MachineOperand *rs1);
+    void print(std::ofstream &outputFile);
 };
 
 class CmpInst : public MachineInst
 {
 public:
 enum Cmp_Inst {
-    Slt, //有符号比较 slt rd, rs1, rs2
-    Slti,
-    Sltz,
-    Snez
-};
-    CmpInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
+    slt, //有符号比较 slt rd, rs1, rs2
+    slti,//slti rd, rs1, imm
 
+    sltz,//sltz rd, rs1
+    snez,//sne rd, rs1
+};
+private:
+    Cmp_Inst opcode;
+public:
+    CmpInst(MachineBlock *parent, Cmp_Inst opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
+    CmpInst(MachineBlock *parent, Cmp_Inst opcode, MachineOperand *rd, MachineOperand *rs1);
+    void print(std::ofstream &outputFile);
 };
 
 class PseudoInst : public MachineInst
@@ -201,14 +294,15 @@ public:
 enum Pseudo_Inst {
     // Nop, //空指令
     // Auipc,
-    Li, //加载立即数 li rd, imm
-    La, //加载地址 la rd, label
-    Mv, //移动 mv rd, rs1
-    Ret //返回 ret
+    mv, //移动 mv rd, rs1
+    ret //返回 ret
 };
-    PseudoInst(int opcode, MachineOperand *rd, MachineOperand *rs1, MachineOperand *rs2);
-    void print();
-
+private:
+    Pseudo_Inst opcode;
+public:
+    PseudoInst(MachineBlock *parent, Pseudo_Inst opcode, MachineOperand *rd, MachineOperand *rs1);
+    PseudoInst(MachineBlock *parent, Pseudo_Inst opcode);
+    void print(std::ofstream &outputFile);
 };
 
 class MachineBlock
