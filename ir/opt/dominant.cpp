15ebf1e1b3811f174eec4e0b663a3e041ba3fdf5
lyh552506 <1661650252@qq.com>
2023-12-01 11:36:03 +0800

    <upd> dominant algorithm has been finished

---
+++ ir/opt/dominant.cpp
@@ -0,0 +1,9 @@
+#include"dominant.hpp"
+
+void dom_begin(){
+    int n,m;//CFG的结点数和边数
+    dominance Dom{n,m};
+    Dom.Init(m);//记录有向边的关系
+    Dom.DFS(1);//起始节点的序号记为1
+    Dom.find_dom();//寻找支配节点
+}
