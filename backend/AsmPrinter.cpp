a836acec86c53c6e094466185fa627e014671fdd
NanQin555 <31352470642qq.com>
2023-12-03 18:07:30 +0800

    <feature>: 完成部分功能

--- backend/AsmPrinter.cpp
+++ backend/AsmPrinter.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
 #include<fstream>
 #include"MachineCode.hpp"
+#include"AsmPrinter.hpp"
 
-void printToLL() {
+void AsmPrinter::PrintCode(MachineUnit* Unit, std::ofstream &outputFile) {
+    for (auto Func : Unit->getFuncList()) {
+        //打印每个Func，及栈帧
+        Func->PrintInstStack();
+        for (auto Block : Func->getBlockList()) {
+            //打印每个Block，计算偏移量
+            
+            for (auto Inst : Block->getInstList()) {
+                //打印每个Inst
+
+            }
+        }
+    }
+}
+
+void AsmPrinter::PrintCodeToTxt(MachineUnit* unit) {
     std::ofstream outputFile("output.ll", std::ios::app); // 以追加模式打开文件
-    LoadInst inst = LoadInst(nullptr, LoadInst::lw, new MachineOperand(MachineOperand::REG, 1), new MachineOperand(MachineOperand::REG, 2));
     if (outputFile.is_open()) {
-        inst.print(outputFile);
-        
+        PrintCode(unit, outputFile);
         outputFile.close(); // 关闭文件
     } else {
         std::cout << "Unable to open the file." << std::endl;
     }
-}
-
-int main() {
-    printToLL();
-    return 0;
 }
