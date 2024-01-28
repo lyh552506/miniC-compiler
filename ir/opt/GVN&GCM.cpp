b2e565a02d7fd8b53a446df0ab50aee956f45ea2
lyh552506 <1661650252@qq.com>
2024-01-28 18:31:58 +0800

    <upd> normal upd

--- ir/opt/GVN&GCM.cpp
+++ ir/opt/GVN&GCM.cpp
@@ -1,22 +1,34 @@
-#include"GVN&GCM.hpp"
+#include "GVN&GCM.hpp"
 
-
-void Gvn_Gcm::init_pass(){
-   
+void Gvn_Gcm::init_pass() {
+  BasicBlock* Entry=m_func->front_block();
+  caculateRPO(Entry);
+  /*start to Gvn*/
 }
 
-void Gvn_Gcm::GVN(){
+void Gvn_Gcm::GVN() {}
 
+
+void Gvn_Gcm::caculateRPO(BasicBlock* bb){
+    if(visited.insert(bb).second){
+      auto& node=m_dom->GetNode(bb->num);
+      for(auto child:node.des){
+        caculateRPO(m_dom->GetNode(child).thisBlock);
+      }
+      RPO.push_back(bb);
+    }
 }
 
-void Gvn_Gcm::GCM(){
-    
-}
 
-void Gvn_Gcm::Schedule_Early(User* inst){
+void Gvn_Gcm::GCM() {}
 
-}
+void Gvn_Gcm::Schedule_Early(User *inst) {}
 
-void Gvn_Gcm::Schedule_Late(User* inst){
+void Gvn_Gcm::Schedule_Late(User *inst) {}
 
+bool Gvn_Gcm::IsPinned(User *inst) {
+  if (dynamic_cast<PhiInst *>(inst) || dynamic_cast<CondInst *>(inst) ||
+      dynamic_cast<UnCondInst *>(inst) || dynamic_cast<RetInst *>(inst))
+      return true;
+  return false;
 }
--- ir/opt/GVN&GCM.hpp
+++ ir/opt/GVN&GCM.hpp
@@ -6,17 +6,18 @@ private:
   std::unordered_map<std::string, User *> Hash_Tab;
   std::unordered_map<std::string,int> ValueToNumer; //VN array
   std::set<User *> PinnedInst;
-
+  Function* m_func;
+  std::vector<BasicBlock*> RPO;
+  std::unordered_set<BasicBlock*> visited;
 public:
-  Gvn_Gcm(dominance *dom) : m_dom(dom), Hash_Tab{}, PinnedInst{} {}
+  Gvn_Gcm(dominance *dom,Function* func) : m_dom(dom), Hash_Tab{}, PinnedInst{},m_func(func) 
+  {}
 
   void GCM();
-
   void GVN();
-
   void init_pass();
-
   void Schedule_Early(User *inst);
-
   void Schedule_Late(User *inst);
+  bool IsPinned(User* inst);
+  void caculateRPO(BasicBlock* bb);
 };
