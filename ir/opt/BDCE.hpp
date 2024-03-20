f980a7e8ef42c7dfe573a926353a9d6386d749c2
Raganhar <top.ranganhar@gmail.com>
2024-03-20 08:45:54 +0800

    merge

--- ir/opt/BDCE.hpp
+++ ir/opt/BDCE.hpp
@@ -6,6 +6,7 @@ public:
     void Pass();
     DCE() = default;
 private:
+    Function* func;
     bool RunOnBlock(BasicBlock* block);
     bool isDeadInst(User* inst);
     bool DCEInst(User* inst, std::vector<User*> Worklist);
