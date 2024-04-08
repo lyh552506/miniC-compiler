f8c9206cf656625f5af427155839e691f142eb2a
NanQin555 <31352470642qq.com>
2024-04-08 22:19:13 +0800

    <update>: 更新后端测试脚本

--- BackendTest/modify_test.sh
+++ BackendTest/modify_test.sh
@@ -1,7 +1,7 @@
 # for each testcases add or del the syslib functions;
 option=$1
 if [ $1 = "-add" ]; then
-    for file in ../test_cases/*.c
+    for file in ../testcases_c/*.c
     do 
         sed -i '1i extern void putint(int a);' "$file"
         sed -i '2i extern void putch(int a);' "$file"
@@ -13,14 +13,29 @@ if [ $1 = "-add" ]; then
         sed -i '8i extern int getch();' "$file"
         sed -i '9i extern int getarray(int a[]);' "$file"
         sed -i '10i extern float getfloat();' "$file"
-        sed -i '11i extern int getfarray(float a[]);' "$file"        
-
+        sed -i '11i extern int getfarray(float a[]);' "$file"
+        sed -i '12i #define starttime() _sysy_starttime(__LINE__);' "$file"
+        sed -i '13i #define stoptime()  _sysy_stoptime(__LINE__);' "$file"
+        sed -i '14i #define _SYSY_N 1024' "$file"
+        sed -i '15i extern void before_main();' "$file"
+        sed -i '16i extern void after_main();' "$file"
+        sed -i '17i extern void _sysy_starttime(int lineno);' "$file"
+        sed -i '18i extern void _sysy_stoptime(int lineno);' "$file"
     done
 elif [ $1 = "-del" ]; then
-    for file in ../test_cases/*.c
+    for file in ../testcases_c/*.c
     do
-            sed -i '1,11d' "$file"
+            sed -i '1,18d' "$file"
     done
+
+elif [ $1 = "-rename" ]; then
+    for file in ../testcases_c/*.sy
+    do
+        base=$(basename $file)
+        base=${base%.*}
+        mv $file ../testcases_c/$base.c
+    done
+
 else 
     echo "Invalid option"
 fi
