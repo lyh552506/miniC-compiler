58022da0c61b30d02fce87b5e2794f5f7f537c66
NanQin555 <31352470642qq.com>
2024-05-07 21:15:47 +0800

    <save>

--- function_test/test.s
+++ function_test/test.s
@@ -1,28 +1,24 @@
-<<<<<<< HEAD
     .file  "file"
     .attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
     .attribute unaligned_access, 0
     .attribute stack_align, 16
-    .text
-    .globl  def_arrf
-    .data
-    .align  3
-    .type  def_arrf, @object
-    .size  def_arrf, 144
-def_arrf:
-    .word  1078867263
-    .zero  140
     .text
     .align  1
     .globl  main
     .type  main, @function
 main:
 .LBB0:
-	lui %1, %hi(.LC0)
-	flw %2, %lo(.LC0)(%1)
-	fsw 0x4009c28f60000000, %0
-	lw a0, 0
+	li %0, 1
+	sw %0, ---i32*FrameObject:.2---
+	li %1, 98799616
+	addiw %1, %1, -837
+	sw %1, ---i32*FrameObject:.5---
+	lw %2, ---i32*FrameObject:.2---
+	lw %3, ---i32*FrameObject:.5---
+	addw %4, %2, %3
+	sw %4, ---i32*FrameObject:.2---
+	li %5, 989892608
+	addiw %5, -902
+	mv a0, %5
 	ret 
     .size main, -main
-=======
->>>>>>> 8426a32ab0abd15daba0cd31fccad1970714fc77
