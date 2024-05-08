f2a45624b4f3905e7aa5dc9893818eee0e694308
Ranganhar <top.ranganhar@gmail.com>
2024-05-08 13:41:04 +0800

    [SCCP]

--- include/ir/opt/SCCP.hpp
+++ include/ir/opt/SCCP.hpp
@@ -6,6 +6,7 @@ static inline int getint(Value* val) { return dynamic_cast<ConstIRInt*>(val)->Ge
 static inline float getfloat(Value* val) { return dynamic_cast<ConstIRFloat*>(val)->GetVal(); }
 static inline bool getbool(Value* val) { return dynamic_cast<ConstIRBoolean*>(val)->GetVal(); }
 
+class InstVisitor;
 struct ValueStatus
 {
     enum Status { BOT, CONST, TOP};
@@ -36,22 +37,48 @@ class ValueMap
 class SCCP
 {
     protected:
+    const std::string name = "SCCP";
     ValueMap val_map;
     std::vector<std::pair<BasicBlock*, BasicBlock*>> cfg_worklist;
     std::vector<User*> SSA_worklist;
+    std::set<std::pair<BasicBlock*, BasicBlock*>> marked;
     private:
     Function* func;
     dominance* dom;
     ConstantFolding* ConstFold;
 
+    void ReplaceConst();
     public:
     SCCP(Function* func, dominance* dom): func(func), dom(dom) {}
     void RunOnFunction();
+    std::set<std::pair<BasicBlock*, BasicBlock*>>& getmarked() { return marked; }
+    ValueStatus getval_status(Value* val) { return val_map.get(val); }  //getmapped
+    ValueMap& getvalue_map() { return val_map; } 
+    std::vector<std::pair<BasicBlock*, BasicBlock*>>& getcfg_worklist() { return cfg_worklist; }
+    std::vector<User*>& getSSA_worklist() { return SSA_worklist; }
+    std::unique_ptr<InstVisitor> visitor;
 };
 
 class InstVisitor
 {
+    private:
     SCCP& sccp;
     ValueMap& value_map;
     std::vector<std::pair<BasicBlock*, BasicBlock*>>& cfg_worklist;
+    std::vector<User*>& SSA_worklist;
+
+    User* inst;
+    BasicBlock* block;
+    ValueStatus prev_statu;
+    ValueStatus cur_statu;
+
+    void visit_Phi(PhiInst* phi);
+    void visit_Branch(CondInst* br);
+    void visit_Branch(UnCondInst* br);
+    void visit_canfold(User* inst);
+
+    public:
+    explicit InstVisitor(SCCP& Sccp) : sccp(Sccp), value_map(Sccp.getvalue_map()), 
+            cfg_worklist(Sccp.getcfg_worklist()), SSA_worklist(Sccp.getSSA_worklist()) {}
+    void visit(User* inst);
 };
--- ir/opt/SCCP.cpp
+++ ir/opt/SCCP.cpp
@@ -1,1 +1,103 @@
 #include "SCCP.hpp"
+
+void SCCP::RunOnFunction()
+{
+    val_map.clear();
+    marked.clear();
+    cfg_worklist.clear();
+    SSA_worklist.clear();
+
+    cfg_worklist.emplace_back(nullptr, func->front());
+    for(BasicBlock* block : *func)
+    {
+        for(User* inst : *block)
+            val_map.set(inst, {ValueStatus::TOP});
+    }
+    int i, j = 0;
+    while(i < cfg_worklist.size() || j < SSA_worklist.size())
+    {
+        while(i < cfg_worklist.size())
+        {
+            BasicBlock* pred = cfg_worklist[i].first;
+            BasicBlock* succ = cfg_worklist[i].second;
+            i++;
+            if(marked.find({pred, succ}) != marked.end())
+                continue;
+            marked.insert(std::make_pair(pred, succ));
+            for(User* inst : *succ)
+                visitor->visit(inst);
+        }
+        while(j < SSA_worklist.size())
+        {
+            User* inst = SSA_worklist[j];
+            j++;
+            BasicBlock* block_ = inst->GetParent();
+            // for(BasicBlock* Block : block_->getpred())
+            // {
+            //     if(marked.count({Block, block_}) != 0)
+            //     {
+            //         visitor->visit(inst);
+            //         break;
+            //     }
+            // }
+        }
+    }
+    // replaceConst(f);
+}
+
+void SCCP::ReplaceConst()
+{
+    std::vector<User*> Del_Insts;
+
+    for(BasicBlock* block : *func)
+    {
+        for(User* inst : *block)
+        {
+            if(Value* value = getval_status(inst).val)
+            {
+                inst->RAUW(value);
+                Del_Insts.push_back(inst);
+            }
+        }
+    }
+    for(User* inst : Del_Insts)
+    {
+        inst->ClearRelation();
+        inst->EraseFromParent();
+    }
+
+    // cond_br with const cond
+    for(BasicBlock* block : *func)
+    {
+        User* inst = block->back();
+        if(!dynamic_cast<CondInst*>(inst))
+            continue;
+        if(Value* cond = inst->Getuselist()[0]->usee)
+        {
+            if(dynamic_cast<ConstIRBoolean*>(cond)->GetVal())
+            {
+                auto uncond = new UnCondInst(dynamic_cast<BasicBlock*>(inst->Getuselist()[1]->usee));
+                inst->ClearRelation();
+                inst->EraseFromParent();
+                block->push_back(uncond);
+            }
+            else
+            {
+                auto uncond = new UnCondInst(dynamic_cast<BasicBlock*>(inst->Getuselist()[2]->usee));
+                inst->ClearRelation();
+                inst->EraseFromParent();
+                block->push_back(uncond);
+            }
+        }
+
+        // if(CondInst* br = dynamic_cast<CondInst*>(block->back()))
+        // {
+        //     if(Value* value = getval_status(br->getcond()).val)
+        //     {
+        //         br->RAUW(value);
+        //         br->ClearRelation();
+        //         br->EraseFromParent();
+        //     }
+        // }
+    }
+}
