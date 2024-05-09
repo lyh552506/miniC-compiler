b8d66186127c1ea1c9278c86e55928993fba6f37
NanQin555 <31352470642qq.com>
2024-05-09 19:36:53 +0800

    <save>

--- function_test/test.s
+++ function_test/test.s
@@ -1,3 +1,20 @@
+--------LiveInterval--------
+inst0Liveness:
+inst1Liveness:%2---FrameObject---
+inst2Liveness:
+inst3Liveness:---FrameObject---%3
+inst4Liveness:---FrameObject---
+inst5Liveness:%4
+inst6Liveness:---FrameObject---%5
+inst7Liveness:
+inst8Liveness:
+--------Block:.LBB0--------
+%5[6,6]
+%3[3,3]
+%4[5,5]
+---FrameObject---[3,3]
+---FrameObject---[1,1][4,4][6,6]
+%2[1,1]
     .file  "file"
     .attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
     .attribute unaligned_access, 0
@@ -8,17 +25,13 @@
     .type  main, @function
 main:
 .LBB0:
-	li %0, 1
-	sw %0, ---i32*FrameObject:.2---
-	li %1, 98799616
-	addiw %1, %1, -837
-	sw %1, ---i32*FrameObject:.5---
-	lw %2, ---i32*FrameObject:.2---
-	lw %3, ---i32*FrameObject:.5---
-	addw %4, %2, %3
-	sw %4, ---i32*FrameObject:.2---
-	li %5, 989892608
-	addiw %5, -902
-	mv a0, %5
+	li %2, 0
+	sw %2, ---FrameObject---
+	li %3, 1
+	sw %3, ---FrameObject---
+	lw %4, ---FrameObject---
+	addi %5, %4, 1
+	sw %5, ---FrameObject---
+	li a0, 0
 	ret 
     .size main, -main
