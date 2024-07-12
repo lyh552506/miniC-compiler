741b21a9eaaacc3c2f5fa67cb07b8f4d74f384f6
Ranganhar <Top.Ranganhar@gmail.com>
2024-07-12 15:17:22 +0800

    [PM] O1 Can Work Safely

--- util/my_stl.hpp
+++ util/my_stl.hpp
@@ -69,7 +69,7 @@ template <typename T> T PopBack(std::vector<T> &vec) {
   return tmp;
 }
 
-#define PassBegin(curfunc)                        \
+#define PassChangedBegin(curfunc)                 \
     for (auto &func : module->GetFuncTion()) {    \
       curfunc = func.get();                       \
       curfunc->bb_num = 0;                        \
@@ -79,4 +79,11 @@ template <typename T> T PopBack(std::vector<T> &vec) {
         curfunc->GetBasicBlock().push_back(bb);   \
       }                                           \
 
-#define PassEnd }
+#define PassChangedEnd }
+
+
+#define RunLevelPass(PassName, curfunc)                               \
+    for (auto &func : module->GetFuncTion()) {                \
+    curfunc = func.get();                                     \
+    RunImpl<PassName>(curfunc, AM);                           \
+    }
