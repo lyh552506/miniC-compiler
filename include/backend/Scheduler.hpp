22f0f062cea014d7f87c4494219764923dac9d1f
NanQin555 <31352470642qq.com>
2024-08-02 16:44:51 +0800

    <Sched>: finish the computation of latency

--- include/backend/Scheduler.hpp
+++ include/backend/Scheduler.hpp
@@ -1,6 +1,7 @@
 #pragma once 
 #include "../backend/Sched.hpp"
 #include "../backend/RISCVContext.hpp"
+
 class Scheduler {
     std::list<Sunit*> Sequence;
     std::list<Sunit*> Abailable;
@@ -9,19 +10,21 @@ class Scheduler {
 public:
     virtual void  ScheduleOnModule(RISCVLoweringContext&) = 0;
     virtual void ScheduleOnBlock(RISCVBasicBlock*) = 0;
-    virtual void ScheduleOnRegion(mylist<RISCVBasicBlock,RISCVMIR>::iterator, mylist<RISCVBasicBlock,RISCVMIR>::iterator) = 0;
+    virtual void ScheduleOnRegion(mylist_iterator, mylist_iterator, BlockDepInfo*) = 0;
 };
 
 class Pre_RA_Scheduler: public Scheduler {
 public:
     void ScheduleOnModule(RISCVLoweringContext&) override;
     void ScheduleOnBlock(RISCVBasicBlock*) override;
-    void ScheduleOnRegion(mylist<RISCVBasicBlock,RISCVMIR>::iterator, mylist<RISCVBasicBlock,RISCVMIR>::iterator) override;
+    void ScheduleOnRegion(mylist_iterator, mylist_iterator, BlockDepInfo*) override;
 };
 
 class Post_RA_Scheduler: public Scheduler {
 public:
     void ScheduleOnModule(RISCVLoweringContext&) override;
     void ScheduleOnBlock(RISCVBasicBlock*) override;
-    void ScheduleOnRegion(mylist<RISCVBasicBlock,RISCVMIR>::iterator, mylist<RISCVBasicBlock,RISCVMIR>::iterator) override;
+    void ScheduleOnRegion(mylist_iterator, mylist_iterator, BlockDepInfo*) override;
 };
+
+
