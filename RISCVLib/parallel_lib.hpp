bc5757be95db7b1606ddc3c86e489394afc585b0
RicoAfoat <3094209316@qq.com>
2024-08-07 22:41:07 +0800

    [parallel lib] change name of buildins

--- RISCVLib/parallel_lib.hpp
+++ RISCVLib/parallel_lib.hpp
@@ -1,11 +1,15 @@
 void CreateThread()__attribute__((constructor)); 
 
-void NotifyWorker()__attribute((always_inline));
-
 static void WaitBufferWrite()__attribute((always_inline));
 
 static void WaitBufferRead()__attribute((always_inline));
 
 static void WaitTasksCompleted()__attribute((always_inline));
 
-void FenceArgLoaded()__attribute((always_inline));
+extern "C"{
+
+void buildin_NotifyWorker(int,int)__attribute((always_inline));
+
+void buildin_FenceArgLoaded()__attribute((always_inline));
+
+}
