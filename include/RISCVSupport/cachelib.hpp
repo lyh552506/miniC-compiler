071b68b2dcd11fb36cac78c29fe75ef2f1fe49a2
NanQin555 <3135247064@qq.com>
2024-08-20 11:08:48 +0800

    <cachelib>

--- include/RISCVSupport/cachelib.hpp
+++ include/RISCVSupport/cachelib.hpp
@@ -6,7 +6,7 @@ R"(	.text
 	.globl	CacheLookUp
 	.type	CacheLookUp, @function
 CacheLookUp:
-.LFB2001:
+.LFB2001_cache:
 	.cfi_startproc
 	slli	a5,a0,32
 	slli	a1,a1,32
@@ -25,89 +25,89 @@ CacheLookUp:
 	add	a0,a4,a7
 	sext.w	a3,a3
 	addi	a2,a2,1
-	beq	a1,zero,.L23
+	beq	a1,zero,.L23_cache
 	ld	a1,0(a0)
-	beq	a1,a5,.L1
+	beq	a1,a5,.L1_cache
 	add	a1,a3,a2
 	li	t1,1022
 	sext.w	a6,a2
 	addw	a3,a3,a2
-	bgtu	a1,t1,.L25
-.L6:
+	bgtu	a1,t1,.L25_cache
+.L6_cache:
 	slli	a2,a1,32
 	srli	a3,a2,28
 	add	a3,a4,a3
 	lw	a2,12(a3)
-	beq	a2,zero,.L18
+	beq	a2,zero,.L18_cache
 	ld	a2,0(a3)
-	beq	a2,a5,.L17
+	beq	a2,a5,.L17_cache
 	addw	a3,a1,a6
 	li	a2,1022
-	bgtu	a3,a2,.L26
-.L8:
+	bgtu	a3,a2,.L26_cache
+.L8_cache:
 	slli	a1,a3,32
 	srli	a2,a1,28
 	add	a2,a4,a2
 	lw	a1,12(a2)
-	beq	a1,zero,.L14
+	beq	a1,zero,.L14_cache
 	ld	a1,0(a2)
-	beq	a1,a5,.L15
+	beq	a1,a5,.L15_cache
 	addw	a2,a6,a3
 	li	a1,1022
-	bgtu	a2,a1,.L27
-.L9:
+	bgtu	a2,a1,.L27_cache
+.L9_cache:
 	slli	a1,a2,32
 	srli	a3,a1,28
 	add	a3,a4,a3
 	lw	a1,12(a3)
-	beq	a1,zero,.L18
+	beq	a1,zero,.L18_cache
 	ld	a1,0(a3)
-	beq	a5,a1,.L17
+	beq	a5,a1,.L17_cache
 	addw	a3,a6,a2
 	li	a2,1022
-	bgtu	a3,a2,.L28
-.L10:
+	bgtu	a3,a2,.L28_cache
+.L10_cache:
 	slli	a2,a3,32
 	srli	a3,a2,28
 	add	a3,a4,a3
 	lw	a2,12(a3)
-	beq	a2,zero,.L18
+	beq	a2,zero,.L18_cache
 	ld	a2,0(a3)
-	beq	a2,a5,.L17
+	beq	a2,a5,.L17_cache
 	add	a4,a4,a7
 	sw	zero,12(a4)
-.L23:
+.L23_cache:
 	sd	a5,0(a0)
-.L1:
+.L1_cache:
 	ret
-.L25:
+.L25_cache:
 	addiw	a1,a3,-1023
-	j	.L6
-.L26:
+	j	.L6_cache
+.L26_cache:
 	addiw	a3,a3,-1023
-	j	.L8
-.L27:
+	j	.L8_cache
+.L27_cache:
 	addiw	a2,a2,-1023
-	j	.L9
-.L28:
+	j	.L9_cache
+.L28_cache:
 	addiw	a3,a3,-1023
-	j	.L10
-.L18:
+	j	.L10_cache
+.L18_cache:
 	mv	a0,a3
 	sd	a5,0(a0)
 	ret
-.L17:
+.L17_cache:
 	mv	a0,a3
 	ret
-.L14:
+.L14_cache:
 	mv	a0,a2
 	sd	a5,0(a0)
 	ret
-.L15:
+.L15_cache:
 	mv	a0,a2
 	ret
 	.cfi_endproc
-.LFE2001:
+.LFE2001_cache:
 	.size	CacheLookUp, .-CacheLookUp
 	.globl	list
 	.bss
